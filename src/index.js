a05ec6ffabd81215122c3dd0269e4a969970b25d
EVGENIY-12 <pet3094@gmail.com>
2022-03-27 19:54:51 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,78 @@
 import './css/styles.css';
+const debounce = require('lodash.debounce');
+import {fetchCountries} from './fetchCountries';
+import countriesMarkupTpl from './templates/country-tpl.hbs';
+import countryMarkupDataTpl from './templates/country-data-tpl.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    countriList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+}
+
+refs.input.addEventListener('input', debounce(onFetcchCountryInfo,DEBOUNCE_DELAY));
+
+function onFetcchCountryInfo(evt) {
+    const searchQuery = evt.target.value.trim()
+
+    fetchCountries(searchQuery).then(countries => {
+
+        console.log(countries);
+
+        if (countries.message === 'Page Not Found' || countries.status === 404) {
+            Notify.failure('Oops, there is no country with that name');
+            makesCountryMarkup('');
+            return
+        }
+      
+        if (countries.length > 10) {
+            Notify.info('Too many matches found. Please enter a more specific name.');
+            makesCountryMarkup('');
+            
+        }
+
+        if (countries.length < 10 && countries.length > 2) {
+            const countryDataFilter = countries.map(getCountriesInfo);
+           
+            makesCountryMarkup(countriesMarkupTpl(countryDataFilter));
+        }
+
+        if (countries.length === 1) {
+            const countryDataFilter = countries.map(getCountryDetales);
+            
+            makesCountryMarkup(countryMarkupDataTpl(countryDataFilter));
+            
+        }
+        
+            
+    })
+        .catch(e => console.log("error"));
+
+    
+}
+
+
+function getCountriesInfo({flags,name}) {
+    return {
+        flag: flags.svg,
+        country: name.official,
+    }
+}
+
+function getCountryDetales({ flags, name, capital, population,languages }) {        
+    return {
+        flags,
+        name,
+        population,
+        capital,
+        languages: Object.values(languages).join(', '),
+    }
+}
+
+function makesCountryMarkup(value) {
+    refs.countryInfo.innerHTML = value
+}
