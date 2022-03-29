af77c8966757c645ee425619607a6f5d5b2491cc
EVGENIY-12 <pet3094@gmail.com>
2022-03-29 15:34:59 +0300

    Update fetch-countries.js

--- src/fetch-countries.js
+++ src/fetch-countries.js
@@ -1,5 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export  function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-    .then(response => response.json())
+        .then(response => {
+
+            if (!response.ok) {
+                Notify.failure('Oops, there is no country with that name');
+                const message = `Произошла ошибка: ${response.status} (${response.statusText})`;
+                throw new Error(message);
+            }
+            
+            return response.json()
+        })
 }
